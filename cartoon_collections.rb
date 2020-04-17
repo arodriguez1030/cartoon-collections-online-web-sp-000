fa324638b9df10b56ae8321c916a560f9670faf2
Ashley Rodriguez <arodriguez1030@gmail.com>
2020-04-17 17:57:10 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -15,6 +15,6 @@ end
 def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
  array.find do |snack|
-   snack.include?(cheese_types)
+   cheese_types.include?(snack)
  end
 end
